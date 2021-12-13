b95659210e444b1d0fa296d98ad72d49074b7767
Onkiponki <pavle.georgijev@gmail.com>
2021-12-13 21:10:23 +0100

    UredjenaForma

--- FirmaForma/Form1.Designer.cs
+++ FirmaForma/Form1.Designer.cs
@@ -29,13 +29,245 @@ namespace FirmaForma
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.idText = new System.Windows.Forms.TextBox();
+            this.nazivText = new System.Windows.Forms.TextBox();
+            this.pibText = new System.Windows.Forms.TextBox();
+            this.adresaText = new System.Windows.Forms.TextBox();
+            this.emailText = new System.Windows.Forms.TextBox();
+            this.tekracunText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.topButton = new System.Windows.Forms.Button();
+            this.botButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idText
+            // 
+            this.idText.Location = new System.Drawing.Point(175, 68);
+            this.idText.Name = "idText";
+            this.idText.Size = new System.Drawing.Size(165, 20);
+            this.idText.TabIndex = 0;
+            // 
+            // nazivText
+            // 
+            this.nazivText.Location = new System.Drawing.Point(175, 104);
+            this.nazivText.Name = "nazivText";
+            this.nazivText.Size = new System.Drawing.Size(165, 20);
+            this.nazivText.TabIndex = 1;
+            // 
+            // pibText
+            // 
+            this.pibText.Location = new System.Drawing.Point(175, 141);
+            this.pibText.Name = "pibText";
+            this.pibText.Size = new System.Drawing.Size(165, 20);
+            this.pibText.TabIndex = 2;
+            // 
+            // adresaText
+            // 
+            this.adresaText.Location = new System.Drawing.Point(175, 177);
+            this.adresaText.Name = "adresaText";
+            this.adresaText.Size = new System.Drawing.Size(165, 20);
+            this.adresaText.TabIndex = 3;
+            // 
+            // emailText
+            // 
+            this.emailText.Location = new System.Drawing.Point(175, 212);
+            this.emailText.Name = "emailText";
+            this.emailText.Size = new System.Drawing.Size(165, 20);
+            this.emailText.TabIndex = 4;
+            // 
+            // tekracunText
+            // 
+            this.tekracunText.Location = new System.Drawing.Point(175, 249);
+            this.tekracunText.Name = "tekracunText";
+            this.tekracunText.Size = new System.Drawing.Size(165, 20);
+            this.tekracunText.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Naziv";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "PIB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Adresa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(43, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "E-Mail";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(43, 244);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Tekući račun";
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(130, 315);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 12;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(221, 315);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 13;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(316, 315);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 14;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // rightButton
+            // 
+            this.rightButton.Location = new System.Drawing.Point(265, 354);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(75, 23);
+            this.rightButton.TabIndex = 15;
+            this.rightButton.Text = ">";
+            this.rightButton.UseVisualStyleBackColor = true;
+            // 
+            // leftButton
+            // 
+            this.leftButton.Location = new System.Drawing.Point(175, 354);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(75, 23);
+            this.leftButton.TabIndex = 16;
+            this.leftButton.Text = "<";
+            this.leftButton.UseVisualStyleBackColor = true;
+            // 
+            // topButton
+            // 
+            this.topButton.Location = new System.Drawing.Point(78, 354);
+            this.topButton.Name = "topButton";
+            this.topButton.Size = new System.Drawing.Size(75, 23);
+            this.topButton.TabIndex = 17;
+            this.topButton.Text = "<<";
+            this.topButton.UseVisualStyleBackColor = true;
+            // 
+            // botButton
+            // 
+            this.botButton.Location = new System.Drawing.Point(359, 354);
+            this.botButton.Name = "botButton";
+            this.botButton.Size = new System.Drawing.Size(75, 23);
+            this.botButton.TabIndex = 18;
+            this.botButton.Text = ">>";
+            this.botButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(560, 457);
+            this.Controls.Add(this.botButton);
+            this.Controls.Add(this.topButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tekracunText);
+            this.Controls.Add(this.emailText);
+            this.Controls.Add(this.adresaText);
+            this.Controls.Add(this.pibText);
+            this.Controls.Add(this.nazivText);
+            this.Controls.Add(this.idText);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox idText;
+        private System.Windows.Forms.TextBox nazivText;
+        private System.Windows.Forms.TextBox pibText;
+        private System.Windows.Forms.TextBox adresaText;
+        private System.Windows.Forms.TextBox emailText;
+        private System.Windows.Forms.TextBox tekracunText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button topButton;
+        private System.Windows.Forms.Button botButton;
     }
 }
 
--- FirmaForma/Form1.cs
+++ FirmaForma/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FirmaForma
 {
     public partial class Form1 : Form
     {
+        string CS = "Data source = DESKTOP-HLB8M6E\\SQLEXPRESS; Initial catalog = Firma; Integrated security = true";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
